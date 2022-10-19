4f2b484edf965cdd8f34361c5d5990a10c80e05b
hellodit33 <45536957+hellodit33@users.noreply.github.com>
2022-10-19 17:05:14 +0200

    connected to mongodb

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
 
+const app = express();
+
+app.use(express.json());
+
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(
+    "mongodb+srv://" +
+      process.env.DB_USER_PASS +
+      "@cluster0.oyapy.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(port, () => console.log(`server is running on port ${port}`));
+  })
+  .catch((err) => console.log(err));
